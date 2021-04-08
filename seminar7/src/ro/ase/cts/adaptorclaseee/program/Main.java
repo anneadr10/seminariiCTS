c0afede3fab652a2ef3cf56c8255fb8e9a0e817f
Panait Anamaria <72298833+anneadr10@users.noreply.github.com>
2021-04-08 15:24:02 -0700

    Adaptor

--- seminar7/src/ro/ase/cts/adaptorclaseee/program/Main.java
+++ seminar7/src/ro/ase/cts/adaptorclaseee/program/Main.java
@@ -1,18 +1,19 @@
 package ro.ase.cts.adaptorclaseee.program;
 
 import ro.ase.cts.adaptorClase.clase.AdaptorCreditClase;
+import ro.ase.cts.adaptorClase.clase.CreditAbstract;
 
 public class Main {
 	
 
-	public static void afiseazaInformatiiCredit(AdaptorCreditClase adaptorClase) {
-		adaptorClase.oferaCredit();
+	public static void afiseazaInfoCredit(CreditAbstract creditAbstract ) {
+		creditAbstract.oferaCredit();
 	}
 
 	public static void main(String[] args) {
 	
-		AdaptorCreditClase adaptorClase= new AdaptorCreditClase(50,"Maria");
-		afiseazaInformatiiCredit(adaptorClase);
+		AdaptorCreditClase adaptorClase =  new AdaptorCreditClase(50, "Maria");
+		afiseazaInfoCredit(adaptorClase);
 	
 
 	}
