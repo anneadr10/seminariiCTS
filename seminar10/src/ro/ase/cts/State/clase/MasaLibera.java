b770eaa6871f11443b981c6ac373acb0bc23b897
Panait Anamaria <72298833+anneadr10@users.noreply.github.com>
2021-05-06 17:30:43 -0700

    Command, Chain, State

---
+++ seminar10/src/ro/ase/cts/State/clase/MasaLibera.java
@@ -0,0 +1,16 @@
+package ro.ase.cts.State.clase;
+
+public class MasaLibera implements Stare {
+	@Override
+	public void modificaStare(Masa masa) {
+		if(masa.getStare() instanceof MasaOcupata || masa.getStare() instanceof MasaRezervata) {
+			masa.setStare(this);
+			System.out.println("Masa cu nr "+masa.getNrMasa()+" s-a eliberat");
+		}
+		else {
+			System.out.println("Masa este libera deja");
+		}
+		
+	}
+
+}
---
+++ seminar10/src/ro/ase/cts/State/clase/MasaRezervata.java
@@ -0,0 +1,15 @@
+package ro.ase.cts.State.clase;
+
+public class MasaRezervata implements Stare{
+	@Override
+	public void modificaStare(Masa masa) {
+		if(masa.getStare() instanceof MasaLibera) {
+			masa.setStare(this);
+			System.out.println("Masa cu nr "+masa.getNrMasa()+" s-a rezervat");
+		}
+		else {
+			System.out.println("Masa indisponibila");
+		}
+		
+	}
+}
