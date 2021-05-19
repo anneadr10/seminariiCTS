254e653bc29bd991508e4f9c8fb5222a9c320748
Panait Anamaria <72298833+anneadr10@users.noreply.github.com>
2021-05-19 16:50:07 -0700

    Observer+ Memento+ Template

---
+++ seminar11/src/ro/ase/cts/Template/clase/Main.java
@@ -0,0 +1,16 @@
+package ro.ase.cts.Template.clase;
+
+public class Main {
+
+	public static void main(String[] args) {
+			// Template
+			// ex 14E
+			
+			Spectator spectator=new Spectator("Ian");
+			spectator.intrarePeStadion();
+
+		}
+
+	}
+
+
